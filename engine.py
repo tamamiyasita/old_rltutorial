cef7da279e6647731228f4c5df21b32b14eaf483
tamamiyasita <sigorou@gmail.com>
2019-07-04 03:37:56 +0900

    fov

--- engine.py
+++ engine.py
@@ -38,64 +38,64 @@ def main():
     # フォントの指定と(libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD）でどのタイプのファイルを読み取るのかを伝える
     libtcod.console_set_custom_font("arial10x10.png", libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    # ここで実際に画面を作成する、画面サイズとタイトルとフルスクリーンにするかの真偽値の3つを指定している
-    libtcod.console_init_root(screen_width, screen_height, "libtcod チュートリアル改訂", False)
+    # ここで実際に画面を作成する、画面サイズとタイトルとフルスクリーンとレンダラーと画面の垂直同期を指定している
+    with libtcod.console_init_root(screen_width, screen_height, "libtcod チュートリアル改訂",
+                                    fullscreen=False, renderer=libtcod.RENDERER_SDL2, vsync=False
+                                    )as con:
 
-    # デフォルトのコンソールを指定する
-    con = libtcod.console.Console(screen_width, screen_height)
+        # ゲームマップの初期化
+        game_map = GameMap(map_width, map_height)
+        r_width, r_height = range(game_map.width), range(game_map.height)
+        game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
-    # ゲームマップの初期化
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+        # 視覚の計算
+        fov_recompute = True
 
-    # 視覚の計算
-    fov_recompute = True
+        fov_map = initialize_fov(game_map, r_width, r_height)
 
-    fov_map = initialize_fov(game_map)
+        # ゲームループと呼ばれるもの、ウィンドウを閉じるまでループする
+        while True:
+            # 視覚の計算
+            if fov_recompute:
+                recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-    # ゲームループと呼ばれるもの、ウィンドウを閉じるまでループする
-    while True:
-        # 視覚をループに渡す
-        if fov_recompute:
-            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+            # entityをここから呼び出す
+            render_all(con, entities, game_map, r_height, r_width, fov_map, fov_recompute, screen_width, screen_height, colors)
 
-        # entityをここから呼び出す
-        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+            fov_recompute = False
 
-        fov_recompute = False
+            # 画面上に描画する機能
+            libtcod.console_flush()
 
-        # 画面上に描画する機能
-        libtcod.console_flush()
+            # 移動後のentitiesをスペース（" "）で上書きして消す
+            clear_all(con, entities)
 
-        # 移動後のentitiesをスペース（" "）で上書きして消す
-        clear_all(con, entities)
-
-        # event.get()イテレータをループで回し各キーの押下判定を行う
-        for events in libtcod.event.get():
-            if events.type == "QUIT":
-                raise SystemExit()
-
-            if events.type == "KEYDOWN":
-
-                # handle_keysから各種変数を作っていく
-                action = handle_keys(events)
-
-                move = action.get("move")
-                exit = action.get("exit")
-                fullscreen = action.get("fullscreen")
-                
-                if move:
-                    dx, dy = move  # action.get("move")で取得した値がdx, dyに代入される
-                    if not game_map.is_blocked(player.x + dx, player.y + dy):
-                        player.move(dx, dy)
-                        # moveで動いたあとにfovを計算する
-                        fov_recompute = True
-
-                if exit:
+            # event.get()イテレータをループで回し各キーの押下判定を行う
+            for events in libtcod.event.get():
+                if events.type == "QUIT":
                     raise SystemExit()
 
-                if fullscreen:
-                    libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+                if events.type == "KEYDOWN":
+
+                    # handle_keysから各種変数を作っていく
+                    action = handle_keys(events)
+
+                    move = action.get("move")
+                    exit = action.get("exit")
+                    fullscreen = action.get("fullscreen")
+                    
+                    if move:
+                        dx, dy = move  # action.get("move")で取得した値がdx, dyに代入される
+                        if not game_map.is_blocked(player.x + dx, player.y + dy):
+                            player.move(dx, dy)
+                            # moveで動いたあとにfovを計算する
+                            fov_recompute = True
+
+                    if exit:
+                        raise SystemExit()
+
+                    if fullscreen:
+                        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 # 明示的に実行された時のみmain()関数を実行する
 if __name__ == "__main__":
