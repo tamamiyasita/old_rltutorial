ea086dbb1a63b298d81160991bf01a009009dc33
tamamiyasita <sigorou@gmail.com>
2019-08-06 22:39:42 +0900

    9

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -8,7 +8,9 @@ from components.item import Item
 
 from entity import Entity
 
-from item_functions import heal
+from game_messages import Message
+
+from item_functions import cast_fireball, cast_lightning, heal
 
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
@@ -134,12 +136,26 @@ class GameMap:
             y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                if randint(0, 100) < 50:
+                item_chance = randint(0, 100)
+
+                if item_chance < 50:
                     item_component = Item(use_function=heal, amount=4)
                     item = Entity(x, y, "!", libtcod.violet, "Healing Potion", render_order=RenderOrder.ITEM,
                                   item=item_component)
 
-                    entities.append(item)
+                elif item_chance < 65:
+                    item_component = Item(use_function=cast_fireball, targeting=True, targeting_message=Message(
+                                          "lift-click a target tile for the fireball, or right-click to cancel.", libtcod.light_cyan),
+                                                            damage=12, radius=3)
+                    item = Entity(x, y, "#", libtcod.red, "Fireball Scroll", render_order=RenderOrder.ITEM,
+                                  item=item_component)
+
+                else:
+                    item_component = Item(use_function=cast_lightning, damage=20, maximum_range=5)
+                    item = Entity(x, y, "#", libtcod.yellow, "Lightning Scroll", render_order=RenderOrder.ITEM,
+                                  item=item_component)
+
+                entities.append(item)
 
 
     def is_blocked(self, x, y):
